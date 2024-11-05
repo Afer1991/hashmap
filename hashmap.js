c5f2e0be50b10eff04574303830cf3c93907523b
Af1991 <152735889+Afer1991@users.noreply.github.com>
2024-11-05 10:30:45 -0500

    Add hash func

--- hashmap.js
+++ hashmap.js
@@ -2,4 +2,15 @@ class HashMap {
   constructor() {
     this.table = new Array(16);
   }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.table.length;
+    }
+
+    return hashCode;
+  }
 }
